3a05613392afd56f7ee7c3da09e26b42c35b2175
Ottodebac <nicolasbrtn7@gmail.com>
2023-06-13 11:55:34 +0800

    Fixed syntax errors Home.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -20,7 +20,7 @@ function Contact() {
           <label htmlFor="name">Name</label>
           <input name="name" placeholder="Enter Name" type="text" />
           <label htmlFor="email">Email</label>
-          <input name="email" placeholder="Enter Name" type="email" />
+          <input name="email" placeholder="Enter Email" type="email" />
           <label htmlFor="message">Message</label>
           <textarea rows="6" placeholder="Enter message" name="message">
             {" "}
